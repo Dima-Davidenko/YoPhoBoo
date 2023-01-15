38eabac30291fe9290c88edcf4f5a4c08d4db077
Dmytro Davidenko <dima080885@gmail.com>
2023-01-15 15:19:05 +0200

    add call button on card

--- src/components/ContactCard/ContactCard.tsx
+++ src/components/ContactCard/ContactCard.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Avatar, Box, Button, Card, CardActions, CardContent, Typography } from '@mui/material';
 import RemoveIcon from '@mui/icons-material/DeleteRounded';
-
-import { getColorFromName } from '../../utils/getColorFromName';
-import { getFirstTwoLetters } from '../../utils/getFirstTwoLetters';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import { Avatar, Box, Button, Card, CardActions, CardContent, Typography } from '@mui/material';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { useTypedDispatch } from '../../hooks/useTypedDispatch';
 import { deleteContact } from '../../redux/phonebook/phonebookOperations';
-import UpdateContactFormModal from '../Forms/UpdateContactFormModal/UpdateContactFormModal';
-import { useSelector } from 'react-redux';
 import { selectIsLoading } from '../../redux/phonebook/phonebookSelectors';
+import { getColorFromName } from '../../utils/getColorFromName';
+import { getFirstTwoLetters } from '../../utils/getFirstTwoLetters';
+import UpdateContactFormModal from '../Forms/UpdateContactFormModal/UpdateContactFormModal';
 
 interface IContactCard {
   id: string;
@@ -38,6 +38,11 @@ const ContactCard: React.FC<IContactCard> = ({ id = '', name = '', number = '' }
         </Box>
       </CardContent>
       <CardActions sx={{ justifyContent: 'flex-end' }}>
+        <a href={`tel:+38${number.replace(/[^\d]/g, '')}`} aria-label="call">
+          <Avatar sx={{ backgroundColor: '#ba68c8', mr: 1 }}>
+            <LocalPhoneIcon />
+          </Avatar>
+        </a>
         <UpdateContactFormModal id={id} />
         <Button
           variant="contained"
