1af14df289ca61580dca01787f380286d7db8595
Dmytro Davidenko <dima080885@gmail.com>
2023-02-27 15:44:13 +0200

    bug fix

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,18 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { useTypedDispatch } from '../../hooks/useTypedDispatch';
-import { fetchContacts } from '../../redux/phonebook/phonebookOperations';
 import { selectFilteredContacts } from '../../redux/phonebook/phonebookSelectors';
 import ContactCard from '../ContactCard/ContactCard';
 import css from './Contacts.module.scss';
 
 const Contacts: React.FC = () => {
-  // const dispatch = useTypedDispatch();
   const filteredContacts = useSelector(selectFilteredContacts);
-  // useEffect(() => {
-  //   // console.log('fetchContacts in Contacts');
-  //   dispatch(fetchContacts());
-  // }, [dispatch]);
   return (
     <ul className={css.contactList}>
       {[...filteredContacts].reverse().map(({ id, name, number }) => (
